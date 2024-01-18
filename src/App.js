06d26feb197139c65f53f26ba94e72d356db5580
oscar burbano <oscar10181@hotmail.com>
2024-01-18 15:04:16 -0500

    se han agregado dos componentes -boton y contador- tambien se han agregado dos botones en el momento no tienen visualizacion en la pagina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import imagenClic from './imagenes/imgClic.png';
 
 function App() {
+
+const manejarClic = () =>{
+  console.log("Clic");
+}
+
+const reiniciarContador = () => {
+  console.log("reinicar");
+}
+
   return (
     <div className="App">
-      
+      <div className='logo-contenedor'>
+        <img 
+          className='img-logo'
+          src={imagenClic}
+          alt='logo de la pagina'
+        />
+      </div>
+      <div className='contenedor-principàl'>
+        <Contador numClics= '5' />
 
+        <Boton
+        texto= 'Clic'
+        esBotonDeClic={true}
+        manejarClic={manejarClic}
+        />
+        <Boton
+        texto= 'Reiniciar'
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
